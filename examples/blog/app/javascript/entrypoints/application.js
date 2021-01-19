86d41816d4054a74cf4e9cc3a101d6fb429e64ee
Maximo Mussini <maximomussini@gmail.com>
2021-01-19 19:39:42 -0300

    Rails Example app

---
+++ examples/blog/app/javascript/entrypoints/application.js
@@ -0,0 +1,18 @@
+// Example: Load Rails libraries in Vite.
+//
+// import '@rails/ujs'
+//
+// import Turbolinks from 'turbolinks'
+// import ActiveStorage from '@rails/activestorage'
+//
+// // Import all channels.
+// import.meta.globEager('./**/*_channel.js')
+//
+// Turbolinks.start()
+// ActiveStorage.start()
+
+// Example: Import a stylesheet in app/javascript/index.css
+// import '~/index.css'
+
+console.log('Vite ⚡️ Rails')
+
