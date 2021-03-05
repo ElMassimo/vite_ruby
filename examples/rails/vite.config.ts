69cf460e25ed4942896b9341126b761ba0f8560c
Maximo Mussini <maximomussini@gmail.com>
2021-03-05 09:59:10 -0300

    feat: Add tag helpers that render code for @vitejs/plugin-legacy

--- examples/rails/vite.config.ts
+++ examples/rails/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import ViteLegacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    ViteLegacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
   ],
 })
