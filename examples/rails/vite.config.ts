45c0c86fb49e6626f5e7d86477e63b471002a7da
Maximo Mussini <maximomussini@gmail.com>
2021-03-18 12:45:32 -0300

    chore: Add vite-plugin-full-reload to sample app

--- examples/rails/vite.config.ts
+++ examples/rails/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import ViteLegacy from '@vitejs/plugin-legacy'
+import FullReload from 'vite-plugin-full-reload'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*']),
     ViteLegacy({
       targets: ['defaults', 'not IE 11'],
     }),
