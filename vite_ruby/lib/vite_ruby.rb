d31ee3425f28f3ff2b31aa6a7ae22aba104c1ab7
Maximo Mussini <maximomussini@gmail.com>
2021-07-01 20:44:02 -0300

    chore: Bump default Vite version

--- vite_ruby/lib/vite_ruby.rb
+++ vite_ruby/lib/vite_ruby.rb
@@ -18,8 +18,8 @@ class ViteRuby
   ENV_PREFIX = 'VITE_RUBY'
 
   # Internal: Versions used by default when running `vite install`.
-  DEFAULT_VITE_VERSION = '^2.2.3'
-  DEFAULT_PLUGIN_VERSION = '^2.0.2'
+  DEFAULT_VITE_VERSION = '^2.3.8'
+  DEFAULT_PLUGIN_VERSION = '^2.0.4'
 
   # Internal: Companion libraries for Vite Ruby, and their target framework.
   COMPANION_LIBRARIES = {
