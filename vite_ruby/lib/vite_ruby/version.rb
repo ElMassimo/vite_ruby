43b43de90e94375724be154cbecb1ab873addbc9
Maximo Mussini <maximomussini@gmail.com>
2021-08-16 12:52:29 -0300

    release: vite_ruby@3.0.0.beta.2

--- vite_ruby/lib/vite_ruby/version.rb
+++ vite_ruby/lib/vite_ruby/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ViteRuby
-  VERSION = '3.0.0.beta.1'
+  VERSION = '3.0.0.beta.2'
 
   # Internal: Versions used by default when running `vite install`.
-  DEFAULT_VITE_VERSION = '^2.5.0-beta.2'
-  DEFAULT_PLUGIN_VERSION = '^3.0.0-beta.2'
+  DEFAULT_VITE_VERSION = '^2.5.0'
+  DEFAULT_PLUGIN_VERSION = '^3.0.0-beta.3'
 end
