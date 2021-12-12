9ada2e87c68899e8e1bad368c875f8214036abcc
Maurizio De Santis <desantis.maurizio@gmail.com>
2021-12-12 17:17:41 +0100

    fix: add variable declaration to import.meta.globEager (close #154) (#155)

--- vite_rails/templates/entrypoints/application.js
+++ vite_rails/templates/entrypoints/application.js
@@ -21,7 +21,7 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 // import ActiveStorage from '@rails/activestorage'
 //
 // // Import all channels.
-// import.meta.globEager('./**/*_channel.js')
+const _channels = import.meta.globEager('./**/*_channel.js')
 //
 // Turbolinks.start()
 // ActiveStorage.start()
