fd5303017760dc176b3fb15908f08a16a175c22f
Maximo Mussini <maximomussini@gmail.com>
2022-03-17 15:15:04 -0300

    feat: bump up default vite version to 2.8.6

--- vite_ruby/lib/vite_ruby/version.rb
+++ vite_ruby/lib/vite_ruby/version.rb
@@ -4,6 +4,6 @@ class ViteRuby
   VERSION = '3.0.9'
 
   # Internal: Versions used by default when running `vite install`.
-  DEFAULT_VITE_VERSION = '^2.7.7'
-  DEFAULT_PLUGIN_VERSION = '^3.0.5'
+  DEFAULT_VITE_VERSION = '^2.9.0-beta.3'
+  DEFAULT_PLUGIN_VERSION = '^3.0.8'
 end
