c21f6fa0decbd961f517693d4723721dba8795c3
Maximo Mussini <maximomussini@gmail.com>
2022-03-17 14:51:12 -0300

    chore: bump vite dev dependencies in all examples

--- vite-plugin-ruby/example/app/frontend/App.vue
+++ vite-plugin-ruby/example/app/frontend/App.vue
@@ -16,8 +16,6 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
 
