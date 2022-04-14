d35662356ab6f308e6a99b007a4c4bb7ea95282a
Maximo Mussini <maximomussini@gmail.com>
2022-04-14 00:53:24 -0300

    chore: make version visible in dark theme

--- examples/rails/app/frontend/components/Version.jsx
+++ examples/rails/app/frontend/components/Version.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Version () {
   const url = `https://github.com/ElMassimo/vite_ruby/tree/${process.env.HEROKU_SLUG_COMMIT}/examples/rails`
   return <>
-    <a className="block text-center text-xs my-16 underline" target="_blank" href={ url }>
+    <a className="version block text-center text-xs my-16 underline" target="_blank" href={ url }>
       { process.env.HEROKU_RELEASE_VERSION }
     </a>
   </>
