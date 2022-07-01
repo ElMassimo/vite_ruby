b5416556a1ce659dc8a0b105f4f5aac2b7db93a5
Maximo Mussini <maximomussini@gmail.com>
2022-07-01 14:11:24 -0300

    deps: bump default versions for Vite and vite-plugin-ruby

--- vite_ruby/lib/vite_ruby/version.rb
+++ vite_ruby/lib/vite_ruby/version.rb
@@ -4,6 +4,6 @@ class ViteRuby
   VERSION = '3.1.6'
 
   # Internal: Versions used by default when running `vite install`.
-  DEFAULT_VITE_VERSION = '^2.9.1'
-  DEFAULT_PLUGIN_VERSION = '^3.0.9'
+  DEFAULT_VITE_VERSION = '^3.0.0-beta.5'
+  DEFAULT_PLUGIN_VERSION = '^3.1.0-beta.0'
 end
