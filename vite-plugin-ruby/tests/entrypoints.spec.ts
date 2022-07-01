02a357312618f09555650611492fc30f46049ea0
Maximo Mussini <maximomussini@gmail.com>
2022-07-01 14:48:40 -0300

    test: update test for resolveEntrypointFiles after removing isInside

--- vite-plugin-ruby/tests/entrypoints.spec.ts
+++ vite-plugin-ruby/tests/entrypoints.spec.ts
@@ -3,9 +3,9 @@ import { describe, test, expect } from 'vitest'
 import { defaultConfig, resolveEntrypointFiles } from '@plugin/config'
 import type { ResolvedConfig } from '@plugin/types'
 
-const entrypointsFor = (config: Partial<ResolvedConfig>) => {
-  config = { ...defaultConfig, ...config }
-  return resolveEntrypointFiles(resolve('example'), config.sourceCodeDir, config)
+const entrypointsFor = (overrideConfig: Partial<ResolvedConfig>) => {
+  const { sourceCodeDir = '', ...config } = { ...defaultConfig, ...overrideConfig }
+  return resolveEntrypointFiles(resolve('example'), resolve('example', sourceCodeDir), config)
 }
 
 const expectEntrypoints = (config: Partial<ResolvedConfig>) =>
@@ -13,13 +13,23 @@ const expectEntrypoints = (config: Partial<ResolvedConfig>) =>
 
 describe('resolveEntrypointFiles', () => {
   test('client build', () => {
+    const defaultEntrypoints = [
+      ['entrypoints/app.css', resolve('example/app/frontend/entrypoints/app.css')],
+      ['entrypoints/main.ts', resolve('example/app/frontend/entrypoints/main.ts')],
+      ['entrypoints/sassy.scss', resolve('example/app/frontend/entrypoints/sassy.scss')],
+      ['entrypoints/frameworks/vue.js', resolve('example/app/frontend/entrypoints/frameworks/vue.js')],
+    ]
+
     expectEntrypoints({}).toEqual([
-      ['app/frontend/entrypoints/app.css', resolve('example/app/frontend/entrypoints/app.css')],
-      ['app/frontend/entrypoints/main.ts', resolve('example/app/frontend/entrypoints/main.ts')],
-      ['app/frontend/entrypoints/sassy.scss', resolve('example/app/frontend/entrypoints/sassy.scss')],
-      ['app/frontend/entrypoints/frameworks/vue.js', resolve('example/app/frontend/entrypoints/frameworks/vue.js')],
-      ['app/frontend/images/logo.png', resolve('example/app/frontend/images/logo.png')],
-      ['app/frontend/images/logo.svg', resolve('example/app/frontend/images/logo.svg')],
+      ...defaultEntrypoints,
+      ['images/logo.png', resolve('example/app/frontend/images/logo.png')],
+      ['images/logo.svg', resolve('example/app/frontend/images/logo.svg')],
+    ])
+
+    expectEntrypoints({ additionalEntrypoints: ['app/assets/*.{js,css}'] }).toEqual([
+      ...defaultEntrypoints,
+      ['../assets/external.js', resolve('example/app/assets/external.js')],
+      ['../assets/theme.css', resolve('example/app/assets/theme.css')],
     ])
   })
 
