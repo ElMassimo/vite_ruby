17c89265f2f5e291ed76784aad647c94e2915d96
Maximo Mussini <maximomussini@gmail.com>
2022-07-01 14:08:56 -0300

    feat: allow starting an ESM SSR server from a CJS app

--- vite_ruby/lib/vite_ruby/cli/ssr.rb
+++ vite_ruby/lib/vite_ruby/cli/ssr.rb
@@ -2,17 +2,27 @@
 
 class ViteRuby::CLI::SSR < ViteRuby::CLI::Vite
   DEFAULT_ENV = CURRENT_ENV || 'production'
+  JS_EXTENSIONS = %w[js mjs cjs]
 
   desc 'Run the resulting app from building in SSR mode.'
   executable_options
 
   def call(mode:, inspect: false, trace_deprecation: false)
     ViteRuby.env['VITE_RUBY_MODE'] = mode
+
+    ssr_entrypoint = JS_EXTENSIONS
+      .map { |ext| ViteRuby.config.ssr_output_dir.join("ssr.#{ ext }") }
+      .find(&:exist?)
+
+    unless ssr_entrypoint
+      raise ArgumentError, "No ssr entrypoint found `#{ ViteRuby.config.ssr_output_dir.relative_path_from(ViteRuby.config.root) }/ssr.{#{ JS_EXTENSIONS.join(',') }}`. Have you run bin/vite build --ssr?"
+    end
+
     cmd = [
       'node',
       ('--inspect-brk' if inspect),
       ('--trace-deprecation' if trace_deprecation),
-      ViteRuby.config.ssr_output_dir.join('ssr.js'),
+      ssr_entrypoint,
     ]
     Kernel.exec(*cmd.compact.map(&:to_s))
   end
