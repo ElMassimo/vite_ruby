e69600cf9fa1db2ba4f03e041b7dee418a225523
Maximo Mussini <maximomussini@gmail.com>
2022-10-07 19:04:15 -0300

    chore: add tests for error message

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -58,11 +58,13 @@ class ViteRuby::Test < Minitest::Test
 
 private
 
-  def stub_builder(build_successful:, stale: false, &block)
+  def stub_builder(build_errors: '', build_successful: build_errors.empty?, stale: false, &block)
     ViteRuby::Build.stub_any_instance(:success, build_successful) {
       ViteRuby::Build.stub_any_instance(:stale?, stale) {
-        result = ['stderr', 'stdout', build_successful]
-        ViteRuby::Builder.stub_any_instance(:build_with_vite, result, &block)
+        ViteRuby::Build.stub_any_instance(:errors, build_errors) {
+          result = ['stdout', build_errors, build_successful]
+          ViteRuby::Builder.stub_any_instance(:build_with_vite, result, &block)
+        }
       }
     }
   end
