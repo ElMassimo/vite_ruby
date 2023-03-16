0f40a1de2d29201a32d7d2f679e99b1ed3208246
Máximo Mussini <maximomussini@gmail.com>
2023-03-16 16:59:54 -0300

    feat: help users that visit the vite dev server directly (#346)
    Based on https://github.com/laravel/vite-plugin/pull/57

--- vite-plugin-ruby/tsup.config.ts
+++ vite-plugin-ruby/tsup.config.ts
@@ -1,4 +1,6 @@
+import { copyFileSync } from 'fs'
 import type { Options } from 'tsup'
+
 export const tsup: Options = {
   clean: true,
   dts: true,
@@ -6,4 +8,7 @@ export const tsup: Options = {
   sourcemap: true,
   target: 'node12',
   format: ['esm', 'cjs'],
+  async onSuccess () {
+    copyFileSync('src/dev-server-index.html', 'dist/dev-server-index.html')
+  },
 }
