4a949fde1672b899c7c0382e99a669195c7ea639
Maximo Mussini <maximomussini@gmail.com>
2023-03-16 15:18:10 -0300

    feat: add support for the --profile flag when running vite build
    Closes #332

--- vite_ruby/lib/vite_ruby/cli/build.rb
+++ vite_ruby/lib/vite_ruby/cli/build.rb
@@ -8,6 +8,7 @@ class ViteRuby::CLI::Build < ViteRuby::CLI::Vite
   option(:ssr, desc: 'Build the SSR entrypoint instead', type: :boolean)
   option(:force, desc: 'Force the build even if assets have not changed', type: :boolean)
   option(:watch, desc: 'Start the Rollup watcher and rebuild on files changes', type: :boolean)
+  option(:profile, desc: 'Gather performance metrics from the build ', type: :boolean)
 
   def call(**options)
     super { |args| ViteRuby.commands.build_from_task(*args) }
