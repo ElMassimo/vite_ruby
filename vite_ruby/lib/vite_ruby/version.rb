f063f283f939d15b3c48c1a9b6efcd589fafbaf1
Maximo Mussini <maximomussini@gmail.com>
2023-11-16 18:44:29 -0300

    feat: use vite 5 in new installations

--- vite_ruby/lib/vite_ruby/version.rb
+++ vite_ruby/lib/vite_ruby/version.rb
@@ -4,6 +4,6 @@ class ViteRuby
   VERSION = '3.4.0'
 
   # Internal: Versions used by default when running `vite install`.
-  DEFAULT_VITE_VERSION = '^4.3.0'
-  DEFAULT_PLUGIN_VERSION = '^3.2.0'
+  DEFAULT_VITE_VERSION = '^5.0.0'
+  DEFAULT_PLUGIN_VERSION = '^5.0.0'
 end
