ElMassimo/vite_ruby

commit 8a581c15ff480049bbb14dab1b5a3497308521b5

fix: prevent clean from deleting assets referenced in the manifests

vite_ruby/lib/vite_ruby/commands.rb
@@ -50,7 +50,7 @@ class ViteRuby::Commands
 
     versions
       .each_with_index
-      .drop_while { |(mtime, _), index|
+      .drop_while { |(mtime, _files), index|
         max_age = [0, Time.now - Time.at(mtime)].max
         max_age < age_in_seconds || index < keep_up_to
       }
@@ -143,14 +143,16 @@ private
 
   def versions
     all_files = Dir.glob("#{ config.build_output_dir }/**/*")
-    entries = all_files - config.manifest_paths - current_version_files
+    entries = all_files - config.manifest_paths - files_referenced_in_manifests
     entries.reject { |file| File.directory?(file) }
       .group_by { |file| File.mtime(file).utc.to_i }
       .sort.reverse
   end
 
-  def current_version_files
-    Dir.glob(manifest.refresh.values.map { |value| config.build_output_dir.join("#{ value['file'] }*") })
+  def files_referenced_in_manifests
+    config.manifest_paths.flat_map { |path|
+      JSON.parse(path.read).map { |_, entry| entry['file'] }
+    }.compact.uniq.map { |path| config.build_output_dir.join(path).to_s }
   end
 
   def with_node_env(env)

test/commands_test.rb
@@ -37,31 +37,49 @@ class CommandsTest < ViteRuby::Test
 
   def test_clean
     with_rails_env('test') { |config|
-      manifest = config.build_output_dir.join('manifest.json')
+      manifest = config.build_output_dir.join('.vite/manifest.json')
+      js_file = config.build_output_dir.join('assets/application.js')
 
       # Should not clean, the manifest does not exist.
-      config.build_output_dir.mkdir unless config.build_output_dir.exist?
+      ensure_output_dirs(config)
       refute clean
 
       # Should not clean, the file is recent.
       manifest.write('{}')
+      js_file.write('export {}')
       assert clean_from_task(OpenStruct.new)
       assert_path_exists manifest
+      assert_path_exists js_file
+
+      # Should not clean if directly referenced.
+      manifest.write('{ "application.js": { "file": "assets/application.js" } }')
+      assert clean(keep_up_to: 0, age_in_seconds: 0)
+      assert_path_exists js_file
 
       # Should clean if we remove age restrictions.
+      manifest.write('{}')
       assert clean(keep_up_to: 0, age_in_seconds: 0)
       assert_path_exists config.build_output_dir
-      refute_path_exists manifest
+      refute_path_exists js_file
     }
   end
 
   def test_clobber
     with_rails_env('test') { |config|
-      config.build_output_dir.mkdir unless config.build_output_dir.exist?
-      config.build_output_dir.join('manifest.json').write('{}')
+      ensure_output_dirs(config)
+      config.build_output_dir.join('.vite/manifest.json').write('{}')
       assert_path_exists config.build_output_dir
       clobber
       refute_path_exists config.build_output_dir
     }
+  end
+
+private
+
+  def ensure_output_dirs(config)
+    config.build_output_dir.rmtree rescue nil
+    config.build_output_dir.mkdir unless config.build_output_dir.exist?
+    config.build_output_dir.join('.vite').mkdir unless config.build_output_dir.join('.vite').exist?
+    config.build_output_dir.join('assets').mkdir unless config.build_output_dir.join('assets').exist?
   end
 end

test/engine_rake_tasks_test.rb
@@ -30,7 +30,7 @@ class EngineRakeTasksTest < ViteRuby::Test
 
     within_mounted_app { `bundle exec rake app:vite:build` }
     assert_path_exists app_public_dir
-    assert_path_exists app_public_dir.join('manifest.json')
+    assert_path_exists app_public_dir.join('.vite/manifest.json')
     assert_path_exists app_public_dir.join('assets')
     refute_path_exists app_ssr_dir
 
@@ -39,14 +39,14 @@ class EngineRakeTasksTest < ViteRuby::Test
 
     within_mounted_app { `bundle exec rake app:vite:build_all` }
     assert_path_exists app_ssr_dir.join('ssr.mjs')
-    refute_path_exists app_ssr_dir.join('manifest.json')
-    refute_path_exists app_ssr_dir.join('manifest-assets.json')
+    refute_path_exists app_ssr_dir.join('.vite/manifest.json')
+    refute_path_exists app_ssr_dir.join('.vite/manifest-assets.json')
 
     within_mounted_app { `bundle exec rake app:vite:clean` }
-    assert_path_exists app_public_dir.join('manifest.json') # Still fresh
+    refute Dir.empty?(app_public_dir.join('assets')) # Still fresh
 
     within_mounted_app { `bundle exec rake app:vite:clean[0,0]` }
-    refute_path_exists app_public_dir.join('manifest.json')
+    refute Dir.empty?(app_public_dir.join('assets')) # Still referenced in manifest
 
     within_mounted_app { `bundle exec rake app:vite:clobber` }
     refute_path_exists app_public_dir
@@ -61,7 +61,7 @@ class EngineRakeTasksTest < ViteRuby::Test
 
     within_mounted_app_root { `bin/vite build --mode development` }
     assert_path_exists app_public_dir
-    assert_path_exists app_public_dir.join('manifest.json')
+    assert_path_exists app_public_dir.join('.vite/manifest.json')
     assert_path_exists app_public_dir.join('assets')
 
     within_mounted_app_root { assert_includes `bin/vite version`, ViteRails::VERSION }

commit 2522b376e0aee9983147c0595831d5164d6cc04e

fix: Retain bundled current files (close #438)
This was broken in https://github.com/ElMassimo/vite_ruby/commit/8a581c15ff480049bbb14dab1b5a3497308521b5 with no explanation of why it was being removed.

vite_ruby/lib/vite_ruby/commands.rb
@@ -143,7 +143,7 @@ private
 
   def versions
     all_files = Dir.glob("#{ config.build_output_dir }/**/*")
-    entries = all_files - config.manifest_paths - files_referenced_in_manifests
+    entries = all_files - config.manifest_paths - files_to_retain
     entries.reject { |file| File.directory?(file) }
       .group_by { |file| File.mtime(file).utc.to_i }
       .sort.reverse
@@ -153,6 +153,10 @@ private
     config.manifest_paths.flat_map { |path|
       JSON.parse(path.read).map { |_, entry| entry['file'] }
     }.compact.uniq.map { |path| config.build_output_dir.join(path).to_s }
+  end
+
+  def files_to_retain
+    Dir.glob(files_referenced_in_manifests.map { |path| "#{ path }*" }).flatten
   end
 
   def with_node_env(env)

test/commands_test.rb
@@ -37,30 +37,50 @@ class CommandsTest < ViteRuby::Test
 
   def test_clean
     with_rails_env('test') { |config|
-      manifest = config.build_output_dir.join('.vite/manifest.json')
+      ensure_output_dirs(config)
+
       js_file = config.build_output_dir.join('assets/application.js')
+      js_file.write('export {}')
+
+      # Simulate using vite-plugin-rails & rollup-plugin-gzip to produce
+      # source maps, gzip & brotli compressed versions of the file.
+      source_map_file = config.build_output_dir.join('assets/application.js.map')
+      source_map_file.write('export {}')
+      gzip_file = config.build_output_dir.join('assets/application.js.gz')
+      gzip_file.write('export {}')
+      brotli_file = config.build_output_dir.join('assets/application.js.br')
+      brotli_file.write('export {}')
 
       # Should not clean, the manifest does not exist.
-      ensure_output_dirs(config)
       refute clean
 
-      # Should not clean, the file is recent.
+      manifest = config.build_output_dir.join('.vite/manifest.json')
       manifest.write('{}')
-      js_file.write('export {}')
+
+      # Should not clean, the file is recent.
       assert clean_from_task(OpenStruct.new)
       assert_path_exists manifest
       assert_path_exists js_file
+      assert_path_exists source_map_file
+      assert_path_exists gzip_file
+      assert_path_exists brotli_file
 
       # Should not clean if directly referenced.
       manifest.write('{ "application.js": { "file": "assets/application.js" } }')
       assert clean(keep_up_to: 0, age_in_seconds: 0)
       assert_path_exists js_file
+      assert_path_exists source_map_file
+      assert_path_exists gzip_file
+      assert_path_exists brotli_file
 
       # Should clean if we remove age restrictions.
       manifest.write('{}')
       assert clean(keep_up_to: 0, age_in_seconds: 0)
       assert_path_exists config.build_output_dir
       refute_path_exists js_file
+      refute_path_exists source_map_file
+      refute_path_exists gzip_file
+      refute_path_exists brotli_file
     }
   end
 