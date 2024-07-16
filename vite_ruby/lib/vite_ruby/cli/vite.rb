1d6ee6d1545c680fb4ca20bf7a8153cdd3c0f345
Maximo Mussini <maximomussini@gmail.com>
2024-07-16 20:34:29 -0300

    feat: change default execution to use `npx vite` instead
    closes #462

--- vite_ruby/lib/vite_ruby/cli/vite.rb
+++ vite_ruby/lib/vite_ruby/cli/vite.rb
@@ -5,6 +5,7 @@ class ViteRuby::CLI::Vite < Dry::CLI::Command
 
   def self.executable_options
     option(:mode, default: self::DEFAULT_ENV, values: %w[development production test], aliases: ['m'], desc: 'The build mode for Vite')
+    option(:node_options, desc: 'Node options for the Vite executable', aliases: ['node-options'])
     option(:inspect, desc: 'Run Vite in a debugging session with node --inspect-brk', aliases: ['inspect-brk'], type: :boolean)
     option(:trace_deprecation, desc: 'Run Vite in debugging mode with node --trace-deprecation', aliases: ['trace-deprecation'], type: :boolean)
   end
@@ -15,10 +16,20 @@ class ViteRuby::CLI::Vite < Dry::CLI::Command
     option(:clobber, desc: 'Clear cache and previous builds', type: :boolean, aliases: %w[clean clear])
   end
 
-  def call(mode:, args: [], clobber: false, **boolean_opts)
+  def call(mode:, args: [], clobber: false, node_options: nil, inspect: nil, trace_deprecation: nil, **boolean_opts)
     ViteRuby.env['VITE_RUBY_MODE'] = mode
     ViteRuby.commands.clobber if clobber
+
+    node_options = [
+      node_options,
+      ('--inspect-brk' if inspect),
+      ('--trace-deprecation' if trace_deprecation),
+    ].compact.join(' ')
+
+    args << %(--node-options="#{ node_options }") unless node_options.empty?
+
     boolean_opts.map { |name, value| args << "--#{ name }" if value }
+
     yield(args)
   end
 end
