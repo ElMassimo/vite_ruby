b8dec4f5b516fe2acc63e980b465987c0c2c9c92
Maximo Mussini <maximomussini@gmail.com>
2024-09-05 10:10:20 -0300

    fix: bump up expected plugin version
    closes #493

--- vite_ruby/lib/vite_ruby/version.rb
+++ vite_ruby/lib/vite_ruby/version.rb
@@ -5,5 +5,5 @@ class ViteRuby
 
   # Internal: Versions used by default when running `vite install`.
   DEFAULT_VITE_VERSION = '^5.0.0'
-  DEFAULT_PLUGIN_VERSION = '^5.0.0'
+  DEFAULT_PLUGIN_VERSION = '^5.1.0'
 end
