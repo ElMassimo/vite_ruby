95c247a66d86f15ad9ce783acf92fef96646091b
Maximo Mussini <maximomussini@gmail.com>
2025-04-14 07:59:06 -0300

    feat: upgrade default vite version to 6.2.6

--- vite_ruby/lib/vite_ruby/version.rb
+++ vite_ruby/lib/vite_ruby/version.rb
@@ -4,6 +4,6 @@ class ViteRuby
   VERSION = "3.9.2"
 
   # Internal: Versions used by default when running `vite install`.
-  DEFAULT_VITE_VERSION = "^5.0.0"
-  DEFAULT_PLUGIN_VERSION = "^5.1.0"
+  DEFAULT_VITE_VERSION = "^6.2.6"
+  DEFAULT_PLUGIN_VERSION = "^5.1.1"
 end
