9d8f3b42e9020a1b6d91bf7432899bff0697fc5a
Maximo Mussini <maximomussini@gmail.com>
2026-03-17 15:29:11 -0300

    feat: bump default plugin version

--- vite_ruby/lib/vite_ruby/version.rb
+++ vite_ruby/lib/vite_ruby/version.rb
@@ -5,5 +5,5 @@ class ViteRuby
 
   # Internal: Versions used by default when running `vite install`.
   DEFAULT_VITE_VERSION = "^8.0.0"
-  DEFAULT_PLUGIN_VERSION = "^5.1.1"
+  DEFAULT_PLUGIN_VERSION = "^5.2.0"
 end
